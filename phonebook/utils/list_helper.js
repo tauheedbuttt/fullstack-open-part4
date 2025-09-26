0c5671e718343d7237549dfa6612e95e5e2127e8
tauheedbuttt <tauheedbutt@gmail.com>
2025-09-26 17:41:31 +0300

    feat(blog): 4.4

--- phonebook/utils/list_helper.js
+++ phonebook/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.map((item) => item.likes).reduce((a, b) => a + b, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
